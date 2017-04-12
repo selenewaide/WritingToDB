03dd19d00068c4a834ab2406c69f2a154d37c173
Selene Waide <selene.waide@ucdconnect.ie>
2017-04-12 16:03:56 +0100

    new read bikes script

--- read_bikes_file_static.py
+++ read_bikes_file_static.py
@@ -1,3 +1,8 @@
+'''
+Reads each bike JSON file, and writes data into the static station table.
+Checks for duplicate station/timestamp combinations and ignores these.
+'''
+
 #!/usr/bin/env python3
 
 import json
@@ -11,22 +16,27 @@ connection = pymysql.connect(host='bikeandweather.cnkbtyr1hegq.us-east-1.rds.ama
                              user='admin',
                              password='Conv2017',
                              db='BikeAndWeather')
+
+# Getting the directory name - where the JSON file are
 if len(sys.argv) > 1:
     directory = sys.argv[1]
 else:
     directory = "."
 
-print("Reading files from ", directory)
+print("Reading files from ", directory) # ensuring we are in the right dir
 
+# Iterating over each file in the directory
 for filename in sorted(os.listdir(directory)):
     suffix_bike = "bikes.JSON"
     if not filename.endswith(suffix_bike):
         continue
-        
-    with open(directory + '/' + filename) as json_bike_data:
-        print(filename)
+    
+    # get data  
+    with open(directory + '/' + filename) as json_bike_data: # looks at files in file timestamp order
+        print(filename) # to give a visual check
         bike_data = json.load(json_bike_data)
         
+        # gets the max timestamp for each station - to avoid duplicates from previous files read in
         for each_station in bike_data:
             with connection.cursor() as cursor:
                 get_max_timestamp = "SELECT MAX(last_update) AS max_num  FROM BikeAndWeather.StationsDynamicTestCopy WHERE station = " + str(each_station['number'])
@@ -38,19 +48,17 @@ for filename in sorted(os.listdir(directory)):
                 max_timestamp = 0
             max_timestamp = float(max_timestamp)
         
-            if (each_station['last_update']/1000) > max_timestamp:
+            if (each_station['last_update']/1000) > max_timestamp: # dividing by 1000 converts time stamp into milliseconds (conventional)
                 
                 with connection.cursor() as cursor:
                     # Create a new record
-                    sql = "INSERT INTO `StationsDynamicTestCopy` (`station`, `status`, `available_bike_stands`,`available_bikes`, `last_update`) VALUES (%s, %s, %s, %s, %s)"
-                    cursor.execute(sql, (each_station['number'], each_station['status'], each_station['available_bike_stands'], each_station['available_bikes'],(each_station['last_update']/1000)))
-                
-                    # connection is not autocommit by default. So you must commit to save
-                    # your changes.
+                    sql = "INSERT INTO `StationsStaticTestCopy` (`station`, `name`, `address`,`lat`, `lng`, `banking`, `bonus`, `contract_name`, `bike_stands`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+                    cursor.execute(sql, (each_station['number'], each_station['name'], each_station['address'], each_station['position']['lat'],  each_station['position']['lng'], each_station['banking'],each_station['bonus'], each_station['contract_name'],each_station['bike_stands']))
+        
+                    # connection is not autocommit by default. Therefore commit to save
+                    # changes.
                     connection.commit()
             else:
                 print("Skipping station ", each_station['number'], " because ", max_timestamp, " is greater than ", each_station['last_update']/1000)
         
-        
-# MAX(last_update) AS max_num  FROM BikeAndWeather.StationsDynamicTestCopy WHERE station = 42      
         
